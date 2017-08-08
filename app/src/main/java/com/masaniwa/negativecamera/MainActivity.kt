e8b7c1e63f0b7b099114d66f09a9b1cffb844655
masaniwa <masaniwasdp@gmail.com>
2017-08-09 04:38:51 +0900

    Runtime Permissionに対応しました。 また、最低バージョンを引き上げました。

--- app/src/main/java/com/masaniwa/negativecamera/MainActivity.kt
+++ app/src/main/java/com/masaniwa/negativecamera/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.masaniwa.negativecamera
 
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.graphics.Bitmap
 import android.graphics.Bitmap.Config.ARGB_8888
 import android.graphics.Bitmap.createBitmap
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat.requestPermissions
+import android.support.v4.content.ContextCompat.checkSelfPermission
 import android.support.v7.app.AppCompatActivity
 import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
-import android.widget.Toast.LENGTH_LONG
+import android.widget.Toast.LENGTH_SHORT
 import android.widget.Toast.makeText
 import com.masaniwa.negativecamera.R.string.*
 import kotlinx.android.synthetic.main.activity_main.*
@@ -26,7 +31,7 @@ import org.opencv.core.Mat
  *
  * @constructor アクティビティを作成する。
  */
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CvCameraViewListener2 {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -34,46 +39,85 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        camera_view.setCvCameraViewListener(cameraListener)
+        camera_view.setCvCameraViewListener(this)
 
         save_button.setOnClickListener { save() }
+
+        save_button.isEnabled = false
     }
 
     override fun onResume() {
         super.onResume()
 
-        if (!initAsync(OPENCV_VERSION_3_2_0, this, loaderCallback)) notice(loading_failed)
+        when (checkSelfPermission(this, CAMERA)) {
+            PERMISSION_GRANTED -> initAsync(OPENCV_VERSION_3_2_0, this, loaderCallback)
+
+            else -> alert(fragmentManager, camera_request) {
+                requestPermissions(this, arrayOf(CAMERA), cameraRequest)
+            }
+        }
     }
 
     override fun onPause() {
+        super.onPause()
+
         camera_view.disableView()
 
-        super.onPause()
+        save_button.isEnabled = false
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onStop() {
+        super.onStop()
 
         camera_view.disableView()
+
+        save_button.isEnabled = false
+    }
+
+    override fun onCameraViewStarted(width: Int, height: Int) {
+        require(width > 0)
+        require(height > 0)
+
+        camera = Mat(height, width, CV_8UC3)
+    }
+
+    override fun onCameraViewStopped() {
+        camera.release()
+    }
+
+    override fun onCameraFrame(frame: CvCameraViewFrame): Mat {
+        when {
+            negative_switch.isChecked -> bitwise_not(frame.rgba(), camera)
+
+            else -> frame.rgba().copyTo(camera)
+        }
+
+        return camera
     }
 
     /**
-     * トーストでテキストを表示する。
+     * トーストで通知する。
      *
-     * @param id 表示するテキストのリソースID。
+     * @param resId 表示する文字列のリソースID。
      */
-    private fun notice(id: Int) {
-        makeText(this, getString(id), LENGTH_LONG).show()
+    private fun notice(resId: Int) {
+        makeText(this, getString(resId), LENGTH_SHORT).show()
     }
 
-    /** ネガポジ反転した画像をストレージに保存する。 */
+    /** カメラの画像をストレージに保存する。 */
     private fun save() {
-        try {
-            savePicture(takeBitmap(camera), directory, contentResolver)
+        when (checkSelfPermission(this, WRITE_EXTERNAL_STORAGE)) {
+            PERMISSION_GRANTED -> try {
+                savePicture(camera.toBitmap(), directory, contentResolver)
 
-            notice(saving_success)
-        } catch (e: StorageException) {
-            notice(saving_failed)
+                notice(saving_success)
+            } catch (e: StorageException) {
+                alert(fragmentManager, saving_failed, null)
+            }
+
+            else -> alert(fragmentManager, storage_request) {
+                requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE), storageRequest)
+            }
         }
     }
 
@@ -81,31 +125,14 @@ class MainActivity : AppCompatActivity() {
     private val loaderCallback = object : BaseLoaderCallback(this) {
         override fun onManagerConnected(status: Int) {
             when (status) {
-                LoaderCallbackInterface.SUCCESS -> camera_view.enableView()
+                LoaderCallbackInterface.SUCCESS -> {
+                    camera_view.enableView()
+
+                    save_button.isEnabled = true
+                }
 
                 else -> super.onManagerConnected(status)
             }
-        }
-    }
-
-    /** カメラビューのイベントリスナ。 */
-    private val cameraListener = object : CvCameraViewListener2 {
-        override fun onCameraViewStarted(width: Int, height: Int) {
-            camera = Mat(height, width, CV_8UC3)
-        }
-
-        override fun onCameraViewStopped() {
-            camera.release()
-        }
-
-        override fun onCameraFrame(frame: CvCameraViewFrame): Mat {
-            when (negative_switch.isChecked) {
-                true -> bitwise_not(frame.rgba(), camera)
-
-                else -> frame.rgba().copyTo(camera)
-            }
-
-            return camera
         }
     }
 
@@ -114,18 +141,27 @@ class MainActivity : AppCompatActivity() {
 }
 
 /**
- * MatからBitmapを生成する。
+ * 自身をBitmapに変換する。
  *
- * @param frame Bitmapの生成元となるMat。
- * @return 生成したBitmap。
+ * @receiver 変換元のMat。
+ * @return 変換したBitmap。
  */
-private fun takeBitmap(frame: Mat): Bitmap {
-    val bitmap = createBitmap(frame.width(), frame.height(), ARGB_8888)
+private fun Mat.toBitmap(): Bitmap {
+    check(width() > 0)
+    check(height() > 0)
 
-    matToBitmap(frame, bitmap)
+    val bitmap = createBitmap(width(), height(), ARGB_8888)
+
+    matToBitmap(this, bitmap)
 
     return bitmap
 }
+
+/** カメラの権限をリクエストするID。 */
+private const val cameraRequest = 0
+
+/** ストレージの権限をリクエストするID。 */
+private const val storageRequest = 1
 
 /** 画像を保存するディレクトリ。 */
 private const val directory = "/NegativeCamera/"
--- app/src/main/java/com/masaniwa/negativecamera/Storage.kt
+++ app/src/main/java/com/masaniwa/negativecamera/Storage.kt
@@ -20,7 +20,7 @@ import java.util.Locale.US
  * @param cause 原因となった例外。
  * @constructor 例外を作成する。
  */
-class StorageException(message: String, cause: Throwable) : IOException(message, cause)
+class StorageException(message: String, cause: Throwable?) : IOException(message, cause)
 
 /**
  * Bitmap画像をjpegでストレージに保存してコンテンツリゾルバに登録する。
@@ -31,9 +31,13 @@ class StorageException(message: String, cause: Throwable) : IOException(message,
  * @throws StorageException 画像を保存できなかった場合。
  */
 fun savePicture(bitmap: Bitmap, directory: String, resolver: ContentResolver) {
+    require(directory != "")
+
     val file = File(getExternalStorageDirectory().path + directory)
 
-    if (!file.exists()) file.mkdir()
+    if (!file.exists()) {
+        if (!file.mkdir()) throw StorageException("Failed to make the directory.", null)
+    }
 
     val name = SimpleDateFormat(format, US).format(Date()) + extension
 
@@ -52,6 +56,8 @@ fun savePicture(bitmap: Bitmap, directory: String, resolver: ContentResolver) {
  * @throws StorageException 画像を保存できなかった場合。
  */
 private fun saveBitmap(bitmap: Bitmap, path: String) {
+    require(path != "")
+
     try {
         FileOutputStream(path).use { bitmap.compress(JPEG, quality, it) }
     } catch (e: IOException) {
@@ -67,6 +73,9 @@ private fun saveBitmap(bitmap: Bitmap, path: String) {
  * @param path ファイルパス。
  */
 private fun saveIndex(resolver: ContentResolver, name: String, path: String) {
+    require(name != "")
+    require(path != "")
+
     val values = ContentValues().apply {
         put(MIME_TYPE, type)
         put(TITLE, name)
