855b1d13468c33f2d1b4c6eaabb85e40de7ed374
masaniwasdp <masaniwasdp@gmail.com>
2021-07-09 06:13:18 +0900

    Aldonita subteno por Android Q.

--- app/src/main/java/io/github/masaniwasdp/crow/infrastructure/ExternalStorage.kt
+++ app/src/main/java/io/github/masaniwasdp/crow/infrastructure/ExternalStorage.kt
@@ -3,12 +3,9 @@ package io.github.masaniwasdp.crow.infrastructure
 import android.content.ContentResolver
 import android.content.ContentValues
 import android.graphics.Bitmap
-import android.os.Environment
 import android.provider.MediaStore
 import io.github.masaniwasdp.crow.presentation.ICameraStorage
-import java.io.File
 import java.io.FileOutputStream
-import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -19,66 +16,35 @@ import java.util.*
  */
 class ExternalStorage(private val resolver: ContentResolver) : ICameraStorage {
     override fun save(bitmap: Bitmap) {
-        reserveDirectory().let {
-            val name = SimpleDateFormat(FORMAT, Locale.US).format(Date()) + EXTENSION
-            val path = it.absolutePath + File.pathSeparator + name
+        val name = SimpleDateFormat(FORMAT, Locale.US).format(Date()) + EXTENSION
 
-            FileOutputStream(path).use { x ->
-                bitmap.compress(Bitmap.CompressFormat.JPEG, QUALITY, x)
-            }
+        val collection = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
 
-            store(resolver, path, name)
+        val newValues = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, name)
+            put(MediaStore.Images.Media.MIME_TYPE, TYPE)
+            put(MediaStore.Images.Media.IS_PENDING, 1)
         }
+
+        val item = resolver.insert(collection, newValues)!!
+
+        resolver.openFileDescriptor(item, "w", null).use {
+            FileOutputStream(it!!.fileDescriptor).use { s ->
+                bitmap.compress(Bitmap.CompressFormat.JPEG, QUALITY, s)
+            }
+        }
+
+        val finalValue = ContentValues().apply { put(MediaStore.Images.Media.IS_PENDING, 0) }
+
+        resolver.update(item, finalValue, null, null)
     }
 }
-
-/**
- * Rezervas dosierujon en stokado por savi dosierojn.
- *
- * @return Dosierujo por savi dosierojn.
- * @throws IOException Kiam malsukcesis rezervi la dosierujon.
- */
-private fun reserveDirectory(): File {
-    return File(Environment.getExternalStorageDirectory().path + DIRECTORY).also {
-        if (!it.exists() && !it.mkdir()) {
-            throw IOException("Failed to make the directory.")
-        }
-    }
-}
-
-/**
- * Registras jpeg-bildon al content-resolver.
- *
- * @param resolver Content-resolver.
- * @param path Vojo de bildo.
- * @param name Nomo de bildo.
- */
-private fun store(resolver: ContentResolver, path: String, name: String) {
-    require(path != "") { "The path must not be empty." }
-    require(name != "") { "The name must not be empty." }
-
-    ContentValues()
-            .apply {
-                put(MediaStore.Images.Media.MIME_TYPE, TYPE)
-                put(MediaStore.Images.Media.TITLE, name)
-                put(DATA_KEY, path)
-            }
-            .let {
-                resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, it)
-            }
-}
-
-/** La dosierujo por savi bildojn. */
-private const val DIRECTORY = "/Crow"
 
 /** La dosiero etendo. */
 private const val EXTENSION = ".jpg"
 
 /** La MIME-tipo de bildoj dosieroj. */
 private const val TYPE = "image/jpeg"
-
-/** La ŝlosilo indikanta datumojn de content-URI. */
-private const val DATA_KEY = "_data"
 
 /** La formato de dato kaj tempo de dosiernomoj kiu estos savita. */
 private const val FORMAT = "yyyyMMdd_hhmmss"
--- app/src/main/java/io/github/masaniwasdp/crow/presentation/ICameraStorage.kt
+++ app/src/main/java/io/github/masaniwasdp/crow/presentation/ICameraStorage.kt
@@ -1,7 +1,6 @@
 package io.github.masaniwasdp.crow.presentation
 
 import android.graphics.Bitmap
-import java.io.IOException
 
 /** Interfaco de la fotila stokado. */
 interface ICameraStorage {
@@ -9,7 +8,6 @@ interface ICameraStorage {
      * Savas bitmap-bildon kiel jpeg-bildo en stokado kaj registras ĝin.
      *
      * @param bitmap Bitmap-bildo kiu estos savita.
-     * @throws IOException Kiam malsukcesis savi la bildon.
      */
     fun save(bitmap: Bitmap)
 }
